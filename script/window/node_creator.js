329dc99cce3165a2e24e38910cb88bdb2689c814
Lampa Pampa <marek.kandulski@proton.me>
2024-03-24 23:40:37 +0100

    added node_creator helper function

---
+++ script/window/node_creator.js
@@ -0,0 +1,13 @@
+function new_node(tag_name, attributes = {}, child_nodes = [], listeners = {})
+{
+    const node = document.createElement(tag_name)
+    for(const attribute in attributes)
+        node[attribute] = attributes[attribute]
+    for(const child of child_nodes)
+        node.appendChild(child)
+    for(const listener_name in listeners)
+        node.addEventListener(listener_name, listeners[listener_name])
+    return node
+}
+
+export default new_node
