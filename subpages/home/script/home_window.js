75001c965791b74f203546aa5c905ecb12892a3c
Lampa Pampa <marek.kandulski@proton.me>
2024-03-31 15:28:28 +0200

    removed kwargs  from home window

--- subpages/home/script/home_window.js
+++ subpages/home/script/home_window.js
@@ -3,20 +3,27 @@ import SessionStorageManager from "../../../script/session_storage_manager.js"
 
 class HomeWindow
 {
-    constructor(main_ui, ui, kwargs)
-    {
+    constructor(
+        main_ui,
+        ui,
+        profession_codes,
+        custom_profession_code,
+        profession_code_to_name,
+        profession_code_storage_key,
+        form_subpage_path
+    ){
         this._main_ui = main_ui
         this._ui = ui
         this._main_ui.create_window()
         this._ui.create_window({
-            profession_codes: kwargs.profession_codes,
-            custom_profession_code: kwargs.custom_profession_code,
-            profession_code_to_name: kwargs.profession_code_to_name,
+            profession_codes: profession_codes,
+            custom_profession_code: custom_profession_code,
+            profession_code_to_name: profession_code_to_name,
             handler: (profession_code) => {
                 this._handle_profession_button_click(
-                    kwargs.profession_code_storage_key,
+                    profession_code_storage_key,
                     profession_code,
-                    kwargs.form_path
+                    form_subpage_path
                 )
             }
         })
--- subpages/home/script/main.js
+++ subpages/home/script/main.js
@@ -16,12 +16,11 @@ function _main()
         new HomeWindowUi(
             config.window.home.ui.section_class_names,
             config.window.home.ui.content,
-        ), {
-            profession_codes: config.window.home.profession_codes,
-            profession_code_to_name: config.profession_code_to_name,
-            custom_profession_code: config.window.home.custom_profession_code,
-            profession_code_storage_key: config.storage_keys.profession_code,
-            form_path: config.form_page_path,
-        }
+        ),
+        config.window.home.profession_codes,
+        config.window.home.custom_profession_code,
+        config.profession_code_to_name,
+        config.storage_keys.profession_code,
+        config.subpage_paths.form,
     )
 }
