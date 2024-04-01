61259c335637f7f144a4df8f08668a2802238b3f
Lampa Pampa <marek.kandulski@proton.me>
2024-04-01 18:47:20 +0200

    added form_values parameter and added subpage_path fields

--- subpages/generator/script/generator_window.js
+++ subpages/generator/script/generator_window.js
@@ -3,10 +3,14 @@ import SessionStorageManager from "../../../script/session_storage_manager.js"
 
 class GeneratorWindow
 {
-    constructor(main_ui, ui, home_subpage_path, form_subpage_path)
+    constructor(main_ui, ui, form_values, home_subpage_path, form_subpage_path)
     {
         this._main_ui = main_ui
         this._ui = ui
+        this._form_values = form_values
+        this._home_subpage_path = home_subpage_path
+        this._form_subpage_path = form_subpage_path
+
         this._main_ui.create_window()
         this._ui.create_window()
         this._main_ui.enable_all_focusable_nodes()
