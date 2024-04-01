8a3b49d57a03422e68496f1d3f824ef074a7c53c
Lampa Pampa <marek.kandulski@proton.me>
2024-04-01 18:37:24 +0200

    added match function

--- script/helpers.js
+++ script/helpers.js
@@ -8,4 +8,18 @@ function make_copy(object)
     return JSON.parse(JSON.stringify(object))
 }
 
-export {route, make_copy}
+function match(object, pattern)
+{
+    if(!object)
+        return false
+    for(const [key, value] of Object.entries(pattern))
+    {
+        if(typeof(value) === "object" && !match(object[key], value)
+            || typeof(value) !== typeof(object[key])
+        )
+            return false
+    }
+    return true
+}
+
+export {route, make_copy, match}
