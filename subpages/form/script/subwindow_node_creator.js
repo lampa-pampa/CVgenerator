4575291fc1f1635bcbf88911bf7cde20438a91fc
Lampa Pampa <marek.kandulski@proton.me>
2024-04-02 23:05:27 +0200

    created SubwindowNodeCreator singleton and added create_multiline_input()

--- subpages/form/script/subwindow_node_creator.js
+++ subpages/form/script/subwindow_node_creator.js
@@ -1,23 +1,42 @@
 import {UiNode} from "../../../script/ui_node.js"
 
-function create_labeled_input(label, args, listeners)
+class SubwindowNodeCreator
 {
-    return new UiNode("li", "", {
-        class: "space-between"
-    }, [
-        new UiNode("label", label),
-        new UiNode("input", "", {
-            class: "text-field border focusable",    
-            ...args,
-        }, [], listeners)
-    ])
+    static create_labeled_input(label, attributes, input_listener)
+    {
+        return new UiNode("li", "", {
+            class: "space-between"
+        }, [
+            new UiNode("label", label),
+            new UiNode("input", "", {
+                class: "text-field border focusable",    
+                ...attributes,
+            }, [], {
+                input: (e) => input_listener(e.target.value?.trim())
+            })
+        ])
+    }
+    
+    static create_multiline_message(message)
+    {
+        return new UiNode("li", "", {}, [
+            new UiNode("pre", message)
+        ])
+    }
+    
+    static create_multiline_input(text_content, attributes, input_listener)
+    {
+        return new UiNode("li", "", {}, [
+            new UiNode("textarea", text_content, {
+                class: "text-field border focusable",
+                spellcheck: false,
+                ...attributes,
+            }, [], {
+                input: (e) => input_listener(e.target.value?.trim())
+            })
+        ])
+    }
 }
 
-function create_multiline_message(message)
-{
-    return new UiNode("li", "", {}, [
-        new UiNode("pre", message)
-    ])
-}
 
-export {create_labeled_input, create_multiline_message}
+export default SubwindowNodeCreator
