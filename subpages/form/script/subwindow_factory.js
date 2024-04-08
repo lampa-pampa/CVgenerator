aab853c30717303ae05e5274d43f71c9c0d6a78d
Lampa Pampa <marek.kandulski@proton.me>
2024-04-08 11:14:05 +0200

    merged code_to_node_creator and code_to_values_validator into code_to_creator_properties

--- subpages/form/script/subwindow_factory.js
+++ subpages/form/script/subwindow_factory.js
@@ -7,8 +7,7 @@ class SubwindowFactory
 {
     #content_class_name
     #code_to_content
-    #code_to_node_creator
-    #code_to_values_validator
+    #code_to_creator_properties
 
     constructor(
         subwindow,
@@ -22,54 +21,80 @@ class SubwindowFactory
         this.#content_class_name = subwindow.content_class_name
         this.#code_to_content = subwindow.code_to_content
         
-        this.#code_to_node_creator = {
-            "1": null,
-            "2": SubwindowNodeCreators.text_field,
-            "3": SubwindowNodeCreators.text_field,
-            "4": null,
-            "5": null,
-            "6": null,
-            "7": SubwindowNodeCreators.text_area,
-            "8": null,
-            "9": null,
-            "10": SubwindowNodeCreators.text_area,
-            "11": SubwindowNodeCreators.text_field,
-            "12": SubwindowNodeCreators.radio_buttons.bind({
-                buttons: theme_code_to_name
-            }),
-            "13": SubwindowNodeCreators.radio_buttons.bind({
-                buttons: layout_code_to_name
-            }),
-            "14": SubwindowNodeCreators.message,
-        }
-
-        this.#code_to_values_validator = {
-            "1": null,
-            "2": SubwindowValuesValidators.all,
-            "3": SubwindowValuesValidators.at_least_one,
-            "4": null,
-            "5": null,
-            "6": null,
-            "7": SubwindowValuesValidators.optional,
-            "8": null,
-            "9": null,
-            "10": SubwindowValuesValidators.optional,
-            "11": SubwindowValuesValidators.all,
-            "12": SubwindowValuesValidators.all,
-            "13": SubwindowValuesValidators.all,
-            "14": SubwindowValuesValidators.optional,
+        this.#code_to_creator_properties = {
+            "1": {
+                node_creator: null,
+                values_validator: null,
+            },
+            "2": {
+                node_creator: SubwindowNodeCreators.text_field,
+                values_validator: SubwindowValuesValidators.all,
+            },
+            "3": {
+                node_creator: SubwindowNodeCreators.text_field,
+                values_validator: SubwindowValuesValidators.at_least_one,
+            },
+            "4": {
+                node_creator: null,
+                values_validator: null,
+            },
+            "5": {
+                node_creator: null,
+                values_validator: null,
+            },
+            "6": {
+                node_creator: null,
+                values_validator: null,
+            },
+            "7": {
+                node_creator: SubwindowNodeCreators.text_area,
+                values_validator: SubwindowValuesValidators.optional,
+            },
+            "8": {
+                node_creator: null,
+                values_validator: null,
+            },
+            "9": {
+                node_creator: null,
+                values_validator: null,
+            },
+            "10": {
+                node_creator: SubwindowNodeCreators.text_area,
+                values_validator: SubwindowValuesValidators.optional,
+            },
+            "11": {
+                node_creator: SubwindowNodeCreators.text_field,
+                values_validator: SubwindowValuesValidators.all,
+            },
+            "12": {
+                node_creator: SubwindowNodeCreators.radio_buttons.bind({
+                    buttons: theme_code_to_name
+                }),
+                values_validator: SubwindowValuesValidators.all,
+            },
+            "13": {
+                node_creator: SubwindowNodeCreators.radio_buttons.bind({
+                    buttons: layout_code_to_name
+                }),
+                values_validator: SubwindowValuesValidators.all,
+            },
+            "14": {
+                node_creator: SubwindowNodeCreators.message,
+                values_validator: SubwindowValuesValidators.optional,
+            },
         }
     }
 
     create(code, ...args)
     {
+        const creator_properties = this.#code_to_creator_properties[code]
         return new Subwindow(
             new SubwindowUi(
                 this.#content_class_name,
                 this.#code_to_content[code],
-                this.#code_to_node_creator[code]
+                creator_properties.node_creator,
             ),
-            this.#code_to_values_validator[code],
+            creator_properties.values_validator,
             ...args,
         )
     }
