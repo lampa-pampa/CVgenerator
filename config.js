19cd6b2a432169c9c6e24b0a7e0b91253451d2e8
Lampa Pampa <marek.kandulski@proton.me>
2024-04-08 22:52:25 +0200

    added buttons section to skills and interests sections

--- config.js
+++ config.js
@@ -244,13 +244,22 @@ const config = {
                             text_field: {
                                 max_length: 32,
                             },
-                            add_button: {
-                                title: "add",
-                                svg: {
-                                    view_box: "0 0 24 24",
-                                    path: "M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z"
+                            buttons: {
+                                add: {
+                                    title: "add",
+                                    svg: {
+                                        view_box: "0 0 24 24",
+                                        path: "M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z"
+                                    },
                                 },
-                            }
+                                remove: {
+                                    title: "remove",
+                                    svg: {
+                                        view_box: "0 0 24 24",
+                                        path: "M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
+                                    },
+                                },
+                            },
                         },
                     },
                     "8": {
@@ -264,13 +273,22 @@ const config = {
                             text_field: {
                                 max_length: 32,
                             },
-                            add_button: {
-                                title: "add",
-                                svg: {
-                                    view_box: "0 0 24 24",
-                                    path: "M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z"
+                            buttons: {
+                                add: {
+                                    title: "add",
+                                    svg: {
+                                        view_box: "0 0 24 24",
+                                        path: "M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z"
+                                    },
                                 },
-                            }
+                                remove: {
+                                    title: "remove",
+                                    svg: {
+                                        view_box: "0 0 24 24",
+                                        path: "M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
+                                    },
+                                },
+                            },
                         },
                     },
                     "9": {
