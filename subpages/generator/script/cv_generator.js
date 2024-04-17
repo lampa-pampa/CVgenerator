9f39c12d3f11f21e3a1b41e39fc373ca38663f2e
Lampa Pampa <marek.kandulski@proton.me>
2024-04-17 15:40:36 +0200

    added for example printing all form data into cv

--- subpages/generator/script/cv_generator.js
+++ subpages/generator/script/cv_generator.js
@@ -6,10 +6,69 @@ class CvGenerator
     {
         return new UiNode({
             tag: "div",
-            text_content: values["2"].name + " " + values["2"].surname,
             attributes: {
-                style: "color: black;"
-            }
+                style: "color: black; width: 100%;"
+            },
+            child_nodes: [
+                new UiNode({
+                    tag: "img",
+                    attributes: {
+                        src: values["1"].image,
+                        style: "width: 150px; height: 200px;"
+                    },
+                }),
+                new UiNode({
+                    tag: "pre",
+                    text_content: values["2"].name + " " + values["2"].surname,
+                }),
+                new UiNode({
+                    tag: "pre",
+                    text_content: values["3"].email + " " + values["3"].phone,
+                }),
+                new UiNode({
+                    tag: "a",
+                    text_content: values["3"].socials,
+                    attributes: {
+                        href: values["3"].socials,
+                    },
+                }),
+                new UiNode({
+                    tag: "pre",
+                    text_content: values["4"].education.join("\n"),
+                }),
+                new UiNode({
+                    tag: "pre",
+                    text_content: values["5"].experience.join("\n"),
+                }),
+                new UiNode({
+                    tag: "pre",
+                    text_content: values["6"].about,
+                }),
+                new UiNode({
+                    tag: "pre",
+                    text_content: values["7"].skills.join("\n"),
+                }),
+                new UiNode({
+                    tag: "pre",
+                    text_content: values["8"].interests.join("\n"),
+                }),
+                new UiNode({
+                    tag: "pre",
+                    text_content: values["9"].info,
+                }),
+                new UiNode({
+                    tag: "pre",
+                    text_content: values["10"].company_name,
+                }),
+                new UiNode({
+                    tag: "pre",
+                    text_content: values["11"].theme_code,
+                }),
+                new UiNode({
+                    tag: "pre",
+                    text_content: values["12"].layout_code,
+                }),
+            ],
         })
     }
 }
